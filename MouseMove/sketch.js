632d0240d62b126784fbfc023bfbda7a43ce620a
Forcesk <emm.castillog@gmail.com>
2018-04-28 18:46:10 -0500

    Mouse move

---
+++ MouseMove/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(700,700);
+}
+
+function draw() {
+  // put drawing code here
+  background(70);
+  ellipse(mouseX,400,15,20);
+  //background(50);
+  ellipse(400,mouseY,50,15,40);
+}
