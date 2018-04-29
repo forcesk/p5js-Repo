b0ff971da3a0611546034318b33ba572cf72ab18
Emmanuel Castillo <emm.castillog@gmail.com>
2018-04-29 10:51:54 -0600

    Random Rain Added.

---
+++ RamdonRain/sketch.js
@@ -0,0 +1,39 @@
+
+var spot =
+{
+  x: 50,
+  y: 50
+}
+
+var col =
+{
+  r: 255,
+  g: 0,
+  b: 0,
+}
+var black = 20;
+
+function setup()
+{
+	createCanvas(600, 400);
+	background(black);
+}
+
+function draw()
+{
+	col.r = random(0,255);
+	col.b = random(0,190);
+	spot.x = random(0, width);
+	spot.y = random(0, height);
+		noStroke();
+
+	fill(col.r,col.g,col.b,100);
+	ellipse(spot.x,spot.y,24,24);
+
+}
+
+function mousePressed()
+{
+	black = black-10;
+	background(black);
+}
